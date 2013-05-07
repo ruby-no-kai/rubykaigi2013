e03b962901f60aa1fc5370abd223fd64db9656df
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-05-08 03:04:33 +0900

    Fix gravatar image size
    The image size is not given to defauld image.

--- config.rb
+++ config.rb
@@ -32,7 +32,7 @@ helpers do
 
   def gravatar_tag(id, size, options = {})
     default_image = CGI.escape('http://rubykaigi.org/2013/commonNoImage.png')
-    image_tag("http://www.gravatar.com/avatar/#{id}?s=#{size}&d=#{default_image}", options)
+    image_tag("http://www.gravatar.com/avatar/#{id}?s=#{size}&d=#{default_image}", options.merge(width: size, height: size))
   end
 
   def link_to_if(condition, text, href, options = {})
